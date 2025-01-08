f50144525cccc3e9da1775c66e06dc07c2e217ba
andonga04 <antonyndonga04@gmail.com>
2025-01-08 07:35:50 +0300

    added insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 // import {
 //   ClerkProvider,
 //   // SignInButton,
@@ -40,6 +40,7 @@ export default function RootLayout({
         >
 
             {children}
+            <SpeedInsights />
 
           <Footer/>
         </body>
