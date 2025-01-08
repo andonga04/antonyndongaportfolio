9c00dcf67383f023129033a6b39bc450819be8e1
andonga04 <antonyndonga04@gmail.com>
2025-01-08 07:50:07 +0300

    deplying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 // import {
 //   ClerkProvider,
 //   // SignInButton,
@@ -41,6 +42,7 @@ export default function RootLayout({
 
             {children}
             <SpeedInsights />
+            <Analytics/>
 
           <Footer/>
         </body>
